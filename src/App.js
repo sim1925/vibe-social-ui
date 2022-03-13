8c7653c908a0cc06e2ce23596d78374ad91c22f7
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-03-13 13:22:16 +0530

    integrated solana-wallet-adapter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,85 @@
 import "./App.css";
-import SidebarWithHeader from "./Components/Sidebar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SimpleSidebar from "./Components/Sidebar";
+
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+    ConnectionProvider,
+    WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+    WalletModalProvider,
+    WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import {
+    LedgerWalletAdapter,
+    PhantomWalletAdapter,
+    SlopeWalletAdapter,
+    SolflareWalletAdapter,
+    SolletExtensionWalletAdapter,
+    SolletWalletAdapter,
+    TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import React, { FC, ReactNode, useMemo } from "react";
+
+require("./App.css");
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 function App() {
     return (
-        <div className="App">
-            <SidebarWithHeader />
-        </div>
+        <Context>
+            <div className="App">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<SimpleSidebar />} />
+                        <Route path="/walletuse" element={<Content />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </Context>
     );
 }
 
 export default App;
+
+const Context: FC<{ children: ReactNode }> = ({ children }) => {
+    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+    const network = WalletAdapterNetwork.Devnet;
+
+    // You can also provide a custom RPC endpoint.
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // of wallets that your users connect to will be loaded.
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+            new SlopeWalletAdapter(),
+            new SolflareWalletAdapter({ network }),
+            new TorusWalletAdapter(),
+            new LedgerWalletAdapter(),
+            new SolletWalletAdapter({ network }),
+            new SolletExtensionWalletAdapter({ network }),
+        ],
+        [network]
+    );
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
+
+export const Content = () => {
+    return (
+        <div className="Button">
+            <WalletMultiButton />
+        </div>
+    );
+};
