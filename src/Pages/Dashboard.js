a01f93fd85b2aa8fe179fa256dde0727adbf4412
Sanskar Mohapatra <sanskarmohapatra@gmail.com>
2022-03-17 17:03:50 +0530

    fixed dashboard bug and added features to ecplore page

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,36 +1,31 @@
-import { HStack, useColorModeValue, VStack } from '@chakra-ui/react'
-import React from 'react'
-import Midsection from '../Components/Midsection'
-import Bottom from '../Components/Bottom'
-import SimpleSidebar from '../Components/sidebar'
-import Searchbar from '../Components/searchbar'
-import Rightpane from '../Components/rightpane'
-
+import { HStack, useColorModeValue, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import Midsection from "../Components/Midsection";
+import Bottom from "../Components/Bottom";
+import SimpleSidebar from "../Components/sidebar";
+import Searchbar from "../Components/searchbar";
+import Rightpane from "../Components/rightpane";
 
 function Dashboard() {
+  const [post, setpost] = useState({
+    publicKey: "",
+    topic: "",
+    content: "",
+  });
+  const [postArray, setpostArray] = useState([]);
   return (
-    <HStack 
-    padding={2}
-    
-    alignItems={"start"}
-    bg={useColorModeValue("#0A131D")}>
-        <SimpleSidebar />
-        <VStack>
-        <Midsection  />
+    <HStack padding={2} alignItems={"start"} bg={useColorModeValue("#0A131D")}>
+      <SimpleSidebar />
+      <VStack>
+        <Midsection />
         <Bottom />
       </VStack>
-        <VStack spacing={5}><Searchbar /> 
-       <Rightpane />
-        </VStack>
-
-
-        
-
-
-        
-        
+      <VStack spacing={5}>
+        <Searchbar />
+        <Rightpane />
+      </VStack>
     </HStack>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
