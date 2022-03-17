9bcc1447b9ee525f4f73dc97fcbf494c2e6eba5e
Sanskar Mohapatra <sanskarmohapatra@gmail.com>
2022-03-17 11:46:06 +0530

    added vibe section

--- src/Components/Bottom.js
+++ src/Components/Bottom.js
@@ -1,35 +1,38 @@
-import React from 'react'
+import React from "react";
 import {
-    IconButton,
-    Box,
-    CloseButton,
-    Flex,
-    Icon,
-    useColorModeValue,
-    Link,
-    Drawer,
-    DrawerContent,
-    Text,
-    useDisclosure,
-    BoxProps,
-    FlexProps,
+  IconButton,
+  Box,
+  CloseButton,
+  Flex,
+  Icon,
+  useColorModeValue,
+  Link,
+  Drawer,
+  DrawerContent,
+  Text,
+  useDisclosure,
+  BoxProps,
+  FlexProps,
 } from "@chakra-ui/react";
+import Post from "./Post";
 
 function Bottom() {
   return (
     <Box
-            bg={useColorModeValue("#11263E")}
-            borderRight="1px"
-            h="76vh"
-            marginTop={2}
-          
-            borderRadius={"10px"}
-            >
-            
-        </Box>
-  )
+      bg={useColorModeValue("#11263E")}
+      borderRight="1px"
+      h="66vh"
+      maxW={"50vw"}
+      marginTop={2}
+      borderRadius={"10px"}
+      overflow="scroll"
+    >
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </Box>
+  );
 }
 
-
-
-export default Bottom
+export default Bottom;
--- src/Components/vibe.js
+++ src/Components/vibe.js
@@ -1,40 +1,37 @@
-import React, { useState } from 'react'
-import {
-  Box,
-  Text,
- Textarea
- 
-} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Input, Text, Textarea } from "@chakra-ui/react";
 
 function Vibe() {
-  const [vibetext, setVibe] = useState('')
+  const [vibetext, setVibe] = useState("");
 
   let handleInputChange = (e) => {
-    let inputValue = e.target.value
-    setVibe(inputValue)
-  }
-  
+    let inputValue = e.target.value;
+    setVibe(inputValue);
+  };
+
   return (
-    <Box
-    >
-    <Textarea
-      value={vibetext}
-      onChange={handleInputChange}
-      width={700} 
-      
-      mx={6}
-      mt={4}
-      placeholder="What's Happening?"
-      size='md'
-      color={"white"}
-     
-    />
-  </Box
-  
-  >
-   
-    
-  )
+    <Box>
+      <Input
+        width={700}
+        mx={6}
+        mt={4}
+        color={"white"}
+        placeholder="Topic"
+        size="md"
+      />
+      <Textarea
+        value={vibetext}
+        onChange={handleInputChange}
+        width={700}
+        mx={6}
+        mt={4}
+        placeholder="What's Happening?"
+        size="md"
+        color={"white"}
+        resize={"none"}
+      />
+    </Box>
+  );
 }
 
-export default Vibe
+export default Vibe;
